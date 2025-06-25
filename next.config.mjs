68803e91bd93f4e290f72d198f867079641f82c0
scmlewis <scmlewis@gmail.com>
2025-06-25 20:45:25 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  basePath: '/intro',
-  assetPrefix: '/intro/',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
